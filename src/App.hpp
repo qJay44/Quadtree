e4677eefe84af449140bdd2d539f9b8ef45b2c03
qJay44 <m70155302@yandex.ru>
2023-10-14 12:44:04 +0300

    new branch

--- src/App.hpp
+++ src/App.hpp
@@ -3,19 +3,53 @@
 #include "myutils.hpp"
 #include "quadtree.hpp"
 #include <stdlib.h>
-#include <vector>
 
-#define WIDTH 800
-#define HEIGHT 800
+#define WIDTH 1280
+#define HEIGHT 960
 
 class App {
+  struct SomeObjectWithArea {
+    sf::Vector2f pos;
+    sf::Vector2f vel;
+    sf::Vector2f size;
+    sf::Color color;
+
+    sf::VertexArray vertices{sf::Quads, 4};
+
+    SomeObjectWithArea(
+      const sf::Vector2f& position,
+      const sf::Vector2f& velocity,
+      const sf::Vector2f& size,
+      const sf::Color& color
+      ) : pos(position), vel(velocity), size(size), color(color) {
+
+      vertices[0].color = this->color;
+      vertices[1].color = this->color;
+      vertices[2].color = this->color;
+      vertices[3].color = this->color;
+    }
+
+    void update() {
+      vertices[0].position = pos;
+      vertices[1].position = {pos.x + size.x, pos.y};
+      vertices[2].position = {pos.x + size.x, pos.y + size.y};
+      vertices[3].position = {pos.x, pos.y + size.y};
+    }
+  };
+
   sf::RenderWindow window;
   sf::Font genericFont;
+  sf::Vector2f mouse{0.f, 0.f};
+  sf::Vector2f mousePressed{0.f, 0.f};
+  sf::View view;
+  sf::View viewStatic;
+  sf::Text countText;
+  sf::Text drawTimeText;
 
-  Rectangle* boundary;
-  QuadTree* qt;
-
-  float mouseX, mouseY;
+  std::vector<SomeObjectWithArea> vecObjects;
+  const float area = 100000.f;
+  const float zoomFactor = 0.3f;
+  float dragFactor = 1.2f;
 
   void setupSFML() {
     // Setup main window
@@ -24,46 +58,91 @@ class App {
 
     // Font for some test text
     genericFont.loadFromFile("../../src/fonts/Minecraft rus.ttf");
+
+    // Rectangles draw count text setup
+    countText.setString("1000000");
+    countText.setFont(genericFont);
+    countText.setCharacterSize(20);
+    countText.setOutlineColor(sf::Color(31, 31, 31));
+    countText.setOutlineThickness(3.f);
+    countText.setPosition({0.f, 0.f});
+
+    // Rectangles draw count text setup
+    drawTimeText.setString("1000 ms");
+    drawTimeText.setFont(genericFont);
+    drawTimeText.setCharacterSize(20);
+    drawTimeText.setOutlineColor(sf::Color(31, 31, 31));
+    drawTimeText.setOutlineThickness(3.f);
+    drawTimeText.setPosition({0.f , countText.getLocalBounds().height + 10.f});
+
+    view.setSize(WIDTH, HEIGHT);
+    view.setCenter(WIDTH / 2.f, HEIGHT / 2.f);
+
+    viewStatic.setSize(WIDTH, HEIGHT);
+    viewStatic.setCenter(WIDTH / 2.f, HEIGHT / 2.f);
   }
 
   void setupProgram() {
     srand((unsigned)time(NULL));
 
-    boundary = new Rectangle{WIDTH / 2.f, HEIGHT / 2.f, WIDTH / 2.f, HEIGHT / 2.f};
-    qt = new QuadTree(*boundary, 4);
+    for (int i = 0; i < 1e6; i++) {
+      SomeObjectWithArea obj(
+        {random(0.f, area), random(0.f, area)},
+        {},
+        {random(0.1f, 100.f), random(0.1f, 100.f)},
+        sf::Color(rand() % 256, rand() % 256, rand() % 256)
+      );
+      vecObjects.push_back(obj);
+    }
+  }
+
+  void onZoom(float direction) {
+    sf::Vector2f prevSize = view.getSize();
+    view.zoom(1.f + direction * zoomFactor);
+    sf::Vector2f currSize = view.getSize();
+
+    float diff = (currSize.x + currSize.y) / (prevSize.x + prevSize.y);
+    dragFactor *= diff;
+  }
+
+  void updateObjects() {
+    for (SomeObjectWithArea& obj : vecObjects)
+      obj.update();
+  }
+
+  void updateView() {
+    view.move((mousePressed - mouse) * dragFactor);
+    mousePressed = mouse;
+    window.setView(view);
   }
 
   void draw() {
-    qt->show(window);
+    sf::VertexArray vertices{sf::Quads};
+    Rectangle screen = {view.getCenter() - view.getSize() / 2.f, view.getSize()};
 
-    Rectangle range{mouseX, mouseY, 50.f, 50.f};
+    int count = 0;
+    sf::Clock clock;
+    for (const SomeObjectWithArea& obj : vecObjects)
+      if (screen.overlaps({obj.pos, obj.size})) {
+        for (int i = 0; i < 4; i++)
+          vertices.append(obj.vertices[i]);
+        count++;
+      }
 
-    sf::RectangleShape rect({range.w * 2, range.h * 2});
-    rect.setPosition(range.x - range.w, range.y - range.h);
-    rect.setFillColor(sf::Color::Transparent);
-    rect.setOutlineColor(sf::Color::Green);
-    rect.setOutlineThickness(1.f);
+    drawTimeText.setString("Draw time: " + std::to_string(clock.restart().asMilliseconds()) + " ms");
+    countText.setString("Count: " + std::to_string(count) + " / " + std::to_string(vecObjects.size()));
 
-    std::vector<Point> found;
-    qt->query(found, range);
+    window.draw(vertices);
 
-    for (const Point& p : found) {
-        sf::CircleShape c(2.f);
-        c.setPosition(p.x, p.y);
-        c.setFillColor(sf::Color::Green);
-        window.draw(c);
-    }
-
-    window.draw(rect);
+    window.setView(viewStatic);
+    window.draw(countText);
+    window.draw(drawTimeText);
   }
 
   public:
     App() {}
 
-    ~App() {
-      delete boundary;
-      delete qt;
-    }
+    ~App() {}
 
     void setup() {
       setupSFML();
@@ -77,17 +156,35 @@ class App {
           if (event.type == sf::Event::Closed)
             window.close();
 
-          if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Key::Q)
-            window.close();
+          if (event.type == sf::Event::KeyReleased)
+            switch (event.key.code) {
+              case sf::Keyboard::Q:
+                window.close();
+                break;
+              default:
+                break;
+            }
 
           if (event.type == sf::Event::MouseMoved) {
-            mouseX = static_cast<float>(sf::Mouse::getPosition(window).x);
-            mouseY = static_cast<float>(sf::Mouse::getPosition(window).y);
+            mouse.x = static_cast<float>(sf::Mouse::getPosition(window).x);
+            mouse.y = static_cast<float>(sf::Mouse::getPosition(window).y);
           }
 
-          if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-            qt->insert({mouseX, mouseY});
+          if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+            mousePressed.x = static_cast<float>(sf::Mouse::getPosition(window).x);
+            mousePressed.y = static_cast<float>(sf::Mouse::getPosition(window).y);
+          }
+
+          if (event.type == sf::Event::MouseWheelScrolled) {
+            if      (event.mouseWheelScroll.delta > 0.f) onZoom(-1);
+            else if (event.mouseWheelScroll.delta < 0.f) onZoom(1);
+          }
         }
+        if (!sf::Mouse::isButtonPressed(sf::Mouse::Left))
+          mousePressed = mouse;
+
+        updateObjects();
+        updateView();
 
         window.clear();
 
--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -2,149 +2,38 @@
 #include <stdexcept>
 #include <vector>
 
-struct Point {
-  float x, y;
-};
-
 struct Rectangle {
-  // NOTE: Coords must point to the center of a rectangle
-  float x, y, w, h;
+  sf::Vector2f pos;
+  sf::Vector2f size;
 
-  float top    = y - h;
-  float right  = x + w;
-  float bottom = y + h;
-  float left   = x - w;
+  Rectangle(const sf::Vector2f& position = { 0.f, 0.f }, const sf::Vector2f& size = { 1.f, 1.f })
+    : pos(position), size(size) {}
 
-  bool contains(const Point& p) const {
-    return (
-      p.x >= x - w &&
-      p.x <= x + w &&
-      p.y >= y - h &&
-      p.y <= y + h
-    );
-  }
-
-  bool intersects(const Rectangle& r) const {
+  constexpr bool contains(const sf::Vector2f& p) const {
     return !(
-      // Checking given rectangle is it off the quad by each side
-      top    > r.bottom || // True: The rectangle under the current quad
-      right  < r.left   || // True: The rectangle to the right of the current quad
-      left   > r.right  || // True: The rectangle to the lefft the current quad
-      bottom < r.top       // True: The rectangle above the current quad
+      p.x < pos.x ||
+      p.y < pos.y ||
+      p.x >= (pos.x + size.x) ||
+      p.y >= (pos.y + size.y)
     );
   }
-};
 
-class QuadTree {
-  Rectangle boundary;
-  int capacity;
-  std::vector<Point> points;
-  bool divided = false;
-
-  QuadTree* northWest = nullptr;
-  QuadTree* northEast = nullptr;
-  QuadTree* southWest = nullptr;
-  QuadTree* southEast = nullptr;
-
-  void subdivide() {
-    if (divided) return;
-    divided = true;
-
-    float& x = boundary.x;
-    float& y = boundary.y;
-    float& w = boundary.w;
-    float& h = boundary.h;
-
-    Rectangle nwRect{x - w / 2, y - h / 2, w / 2, h / 2};
-    Rectangle neRect{x + w / 2, y - h / 2, w / 2, h / 2};
-    Rectangle swRect{x - w / 2, y + h / 2, w / 2, h / 2};
-    Rectangle seRect{x + w / 2, y + h / 2, w / 2, h / 2};
-
-    northWest = new QuadTree(nwRect, capacity);
-    northEast = new QuadTree(neRect, capacity);
-    southWest = new QuadTree(swRect, capacity);
-    southEast = new QuadTree(seRect, capacity);
+  constexpr bool contains(const Rectangle& r) const {
+    return (
+      (r.pos.x >= pos.x) &&
+      (r.pos.y >= pos.y) &&
+      (r.pos.x + r.size.x < pos.x + size.x) &&
+      (r.pos.y + r.size.y < pos.y + size.y)
+    );
   }
 
-  public:
-
-    QuadTree(Rectangle boundary, int capacity)
-      : boundary(boundary), capacity(capacity) {
-        points.reserve(capacity);
-    }
-
-    ~QuadTree() {
-      delete northWest;
-      delete northEast;
-      delete southWest;
-      delete southEast;
-    }
-
-    bool insert(Point p) {
-      if (!boundary.contains(p)) return false;
-
-      if (points.size() < capacity) {
-        points.push_back(p);
-        return true;
-      } else {
-        subdivide();
-        if (
-          northWest->insert(p) ||
-          northEast->insert(p) ||
-          southWest->insert(p) ||
-          southEast->insert(p)
-        ) return true;
-        else
-          throw std::runtime_error("The point somehow didn't inserted in any quad");
-      }
-    }
-
-    void query(std::vector<Point>& found, const Rectangle& range) {
-      if (boundary.intersects(range)) {
-        for (const Point& p : points)
-          if (range.contains(p))
-            found.push_back(p);
-      }
-
-      if (divided) {
-        northWest->query(found, range);
-        northEast->query(found, range);
-        southWest->query(found, range);
-        southEast->query(found, range);
-      }
-    }
-
-    void show(sf::RenderWindow& win) {
-      float& x = boundary.x;
-      float& y = boundary.y;
-      float& w = boundary.w;
-      float& h = boundary.h;
-
-      sf::Vertex lineVertical[] = {
-        sf::Vertex({x, y - h}),
-        sf::Vertex({x, y + h})
-      };
-
-      sf::Vertex lineHorizontal[] = {
-        sf::Vertex({x - w, y}),
-        sf::Vertex({x + w, y})
-      };
-
-      for (Point p : points) {
-        sf::CircleShape c(1.f);
-        c.setPosition(p.x, p.y);
-        win.draw(c);
-      }
-
-      win.draw(lineVertical, 2, sf::Lines);
-      win.draw(lineHorizontal, 2, sf::Lines);
-
-      if (divided) {
-        northWest->show(win);
-        northEast->show(win);
-        southWest->show(win);
-        southEast->show(win);
-      }
-    }
+  constexpr bool overlaps(const Rectangle& r) const {
+    return (
+      pos.x < r.pos.x + r.size.x &&
+      pos.y < r.pos.y + r.size.y &&
+      pos.x + size.x >= r.pos.x  &&
+      pos.y + size.y >= r.pos.y
+    );
+  }
 };
 
